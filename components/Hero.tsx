bd1e2db1cf70b889d5f5acc8c86dcf7bf9582eb2
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-08-19 23:31:34 +0330

    Create hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,22 @@
+"use client";
+import { Button } from ".";
+
 type Props = {};
 
 const Hero = (props: Props) => {
-  return <div>Hero</div>;
+  const handleScroll = () => {};
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        {/* hero title */}
+        <h1 className="hero__title">Find, book, or rent a car -- quickly and easily!</h1>
+        {/* hero subtitle */}
+        <p className="hero__subtitle">Streamline your car rental experience with our effortless booking process.</p>
+        {/* hero btn */}
+        <Button title="Explore Cars" containerStyles="bg-primary-blue text-white rounded-full mt-10" onClick={handleScroll} />
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
