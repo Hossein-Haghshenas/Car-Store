dc425fcb5f6b3085e82054e11fd655d327400d79
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-08-23 20:02:55 +0330

    Create navbar component

---
+++ components/Navbar.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import Image from "next/image";
+// Components
+import { Button } from "@/components";
+
+const Navbar = () => {
+  return (
+    <header className="w-full absolute z-10">
+      <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4">
+        {/* logo */}
+        <Link href="/" className="flex justify-center items-center">
+          <Image className="object-contain" src="/logo.svg" alt="Car Store" width={118} height={18} />
+        </Link>
+        {/* signIn btn */}
+        <Button title="Sighn In" btnType={"button"} containerStyles="text-primary-blue rounded-full bg-white min-w-[130px]" />
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
