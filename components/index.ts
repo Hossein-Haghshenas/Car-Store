3f7966a319631ffe870436f2f48dc5c879404940
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-08-23 20:07:29 +0330

    Import component in index file

--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,6 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 import Hero from "./Hero";
 import Button from "./Button";
 
-export { Hero, Button };
+export { Hero, Button, Navbar, Footer };
