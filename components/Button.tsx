96950e5355fe50bd86f667f46b97fac2d3cf02c7
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-08-23 20:05:09 +0330

    Update button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,9 +1,9 @@
 // types
 import { ButtonProps } from "@/types";
 
-const Button = ({ title, containerStyles, onClick }: ButtonProps) => {
+const Button = ({ title, containerStyles, onClick, btnType }: ButtonProps) => {
   return (
-    <button disabled={false} type={"button"} className={`custom-btn ${containerStyles && containerStyles}`} onClick={onClick}>
+    <button disabled={false} type={btnType || "button"} className={`custom-btn ${containerStyles && containerStyles}`} onClick={onClick}>
       <span className={`flex-1`}>{title}</span>
     </button>
   );
