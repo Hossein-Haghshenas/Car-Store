1fd3361fd0fddc07c2a35f3c9acb20a7aa157116
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-08-29 18:45:49 +0330

    Export components

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,9 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import Hero from "./Hero";
 import Button from "./Button";
+import SearchBar from "./SearchBar";
+import SearchManufacturer from "./SearchManufacturer";
+import CustomFilter from "./CustomFilter";
+import CarCard from "./CarCard";
 
-export { Hero, Button, Navbar, Footer };
+export { Hero, Button, SearchBar, SearchManufacturer, CustomFilter, CarCard, Navbar, Footer };
