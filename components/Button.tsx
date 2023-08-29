aabfa1e912a3b11269a014ea2bf8c042944d4da9
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-08-29 18:36:20 +0330

    Update button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,10 +1,16 @@
+import Image from "next/image";
 // types
 import { ButtonProps } from "@/types";
 
-const Button = ({ title, containerStyles, onClick, btnType }: ButtonProps) => {
+const Button = ({ title, containerStyles, onClick, btnType, textStyles, rightIcon, isDisable }: ButtonProps) => {
   return (
     <button disabled={false} type={btnType || "button"} className={`custom-btn ${containerStyles && containerStyles}`} onClick={onClick}>
-      <span className={`flex-1`}>{title}</span>
+      <span className={`flex-1 ${textStyles}`}>{title}</span>
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image src={rightIcon} alt="right icon" fill className="object-contain" />
+        </div>
+      )}
     </button>
   );
 };
