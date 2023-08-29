e8fe3d9fcf8e660747e44d940696d4c60912a2f1
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-08-29 18:36:04 +0330

    Update types

--- types/index.ts
+++ types/index.ts
@@ -5,4 +5,40 @@ export interface ButtonProps {
   containerStyles?: string;
   onClick?: MouseEventHandler<HTMLButtonElement>;
   btnType?: "button" | "submit";
+  textStyles?: string;
+  rightIcon?: string;
+  isDisable?: boolean;
+}
+
+export interface SearchManufacturer {
+  manufacturer: string;
+  setManufacturer: (manufacturer: string) => void;
+}
+
+export interface CarProps {
+  city_mpg: number;
+  class: string;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
+}
+
+export interface CarDetailsProps {
+  isOpen: boolean;
+  closeModal: () => void;
+  car: CarProps;
+}
+
+export interface CustomFilterTypes {
+  title: string;
+}
+export interface SearchBarTypes {
+  title: string;
 }
