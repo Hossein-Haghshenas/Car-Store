1400a9272dcc8d2b048be79fa22aec8196e94596
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-08-31 18:37:26 +0330

    Pass filters as parameter in fetcher function

--- utils/fetcher.ts
+++ utils/fetcher.ts
@@ -1,9 +1,15 @@
-export const fetchCars = async () => {
+// Types
+import { FilterProps } from "@/types";
+
+export const fetchCars = async (filters: FilterProps) => {
+  // destructure filters
+  const { manufacturer, year, model, limit, fuel } = filters;
+
   const headers = {
     "X-RapidAPI-Key": process.env.RAPIDAPI_KEY || "",
     "X-RapidAPI-Host": process.env.RAPIDAPI_HOST || "",
   };
-  const baseUrl = "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla";
+  const baseUrl = `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`;
 
   try {
     const response = await fetch(baseUrl, { headers: headers });
